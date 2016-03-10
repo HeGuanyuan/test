f5ab86b668214eee64d09a9b6221b950f2e4c8f1
HeGuanyuan <heguanyuan@outlook.com>
2016-03-10 21:25:16 +0800

    优雅地显示

--- src/InComeCal.java
+++ src/InComeCal.java
@@ -1,22 +1,33 @@
+import java.util.Formatter;
+
 public class InComeCal {
 	public static float thisInCome = 0;
-	public static float thisSalary = 0;
-	public static float lastSalary = 4;
-	public static float salaryIncreaseRate = 0.15f;
+	public static float thisSalary = 4;
+	public static float lastSalary = 0;
+	public static float salaryIncreaseRate = 0.20f;
 	public static float investProfitRate = 0.24f;
 	public static float investProfit = 0;
-	public static float total = 4;
+	public static float total = 0;
 	public static int years = 5;
 
 	public static void main(String[] args) {
+		String s = "%5d: 储蓄:%5.1f  利息:%5.1f  资产:%6.1f\n";
+		Formatter f = new Formatter(System.out);
 		for (int i = 0; i < years; i++) {
-			thisSalary = lastSalary * (salaryIncreaseRate + 1);
+
+			if (i == 0) {
+				total = 0;
+				lastSalary = 0;
+			} else {
+				thisSalary = lastSalary * (salaryIncreaseRate + 1);
+			}
+
 			investProfit = total * investProfitRate;
 			thisInCome = thisSalary + investProfit;
 			total = thisInCome + total;
 			lastSalary = thisSalary;
-			String f = "第%3d年 : 储蓄额：%2.1f 利息收入：%3.1f 资产：%3.1f\n";
-			System.out.format(f, i + 2, thisSalary, investProfit, total);
+			f.format(s, i + 1, thisSalary, investProfit, total);
+
 		}
 	}
 }
