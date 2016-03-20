c28648fab8b1f193644668cb49b9723f28c49601
HeGuanyuan <master@heguanyuan.com>
2016-03-20 20:22:54 +0800

    add design patttern -Singleton

---
+++ src/DesignPattern/Singleton.java
@@ -0,0 +1,20 @@
+package DesignPattern;
+
+/*单例模式*/
+public class Singleton {
+	private volatile static Singleton singleton;
+
+	private Singleton() {
+	};
+
+	public static Singleton getInstance() {
+		if (singleton == null) {
+			synchronized (Singleton.class) {
+				if (singleton == null) {
+					singleton = new Singleton();
+				}
+			}
+		}
+		return singleton;
+	}
+}
